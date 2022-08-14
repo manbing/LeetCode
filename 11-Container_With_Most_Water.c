e55029fb49a50219eadfd86d4fb9bf5a4ac24499
Manbing <manbing3@gmail.com>
2022-08-14 08:50:05 -0700

    11. Container With Most Water:
    Signed-off-by: Manbing <manbing3@gmail.com>

---
+++ 11-Container_With_Most_Water.c
@@ -0,0 +1,21 @@
+#define MIN(x,y) (x) < (y)? (x) : (y)
+#define MAX(x,y) (x) > (y)? (x) : (y)
+
+int maxArea(int* height, int heightSize){
+    int left = 0, right = heightSize - 1;
+    int retval = 0;
+    int size = 0;
+    int volume = 0;
+    
+    for (size = heightSize - 1; size > 0; size--) {
+        volume = (MIN(height[left], height[right])) * size;
+        retval = MAX(retval, volume);
+        
+        if (height[left] < height[right])
+            left++;
+        else
+            right--;
+    }
+    
+    return retval;
+}
